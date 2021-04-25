8a9882d0cbe08cf199300158af66a4fd01036aab
Hugor Chau <hchau@su-a4.msk.21-school.ru>
2021-04-25 18:40:16 +0300

    algorithm is not so good, but it works

--- src/swap/algorithm.c
+++ src/swap/algorithm.c
@@ -4,7 +4,7 @@ void	get_a_sorted(t_stack *a)
 {
 	void		(*rotate_a)(t_stack *);
 
-	if (a->begin->index >= a->size)
+	if (a->begin->index <= a->size / 2)
 		rotate_a = &rra;
 	else
 		rotate_a = &ra;
