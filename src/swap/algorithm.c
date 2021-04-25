49dab8241f0162f7f3f21442795d4fc2adf8aa28
Hugor Chau <hchau@su-a4.msk.21-school.ru>
2021-04-25 13:07:37 +0300

    before changes

--- src/swap/algorithm.c
+++ src/swap/algorithm.c
@@ -4,6 +4,14 @@
 
 #include "header.h"
 
+//void		*rotate_a(t_stack *a)
+//{
+//	unsigned int		i;
+//
+//	i = 0;
+//	while (i < )
+//}
+
 void		main_cycle(t_stack *a, t_stack *b)
 {
 	unsigned int			main_size;
@@ -11,5 +19,6 @@ void		main_cycle(t_stack *a, t_stack *b)
 	main_size = a->size;
 	indexing(a);
 	prepare_a(a, b);
+//	rotate_a(a);
 	get_from_b(a, b);
 }
--- src/swap/choose_element.c
+++ src/swap/choose_element.c
@@ -41,36 +41,81 @@ void			count_commands(unsigned int *next, unsigned int *reverse, t_stack *stack,
 	unsigned int		rot;
 	t_node				*cur_max;
 	t_iter				*iter;
+	t_iter				*iter_next;
 
 	i = 0;
 	rot = 0;
+
+	*next = 1;
 	cur_max = stack->begin;
 	iter = new_iter(stack);
+	iter_next = new_iter(stack);
 	while (i < stack->size)
 	{
 		//1) найти максимально большое число, после которого можно поставить ноду
-		if (iter->cur->val >= elem->val)
-		{
-			iter->prev(iter);
-			rot = i - 1;
-			break ;
-		}
+		iter_next->next_iter(iter_next);
 		if (iter->cur->val > cur_max->val)
 		{
-			rot = i;
+			*next = i + 1;
 			cur_max = iter->cur;
+		}
+		if (iter->cur->val < elem->val && iter_next->cur->val >= elem->val)
+		{
+//			iter->prev(iter);
+			cur_max = iter;
+			*next = i;
+			break ;
 		}
 		iter->next_iter(iter);
 		i++;
 	}
-	*next = rot;
+//	*next = rot;
+	iter->prev(iter);
 	*reverse = stack->size - *next;
 	if (*next < *reverse)
 		*reverse = 0;
 	else
 		*next = 0;
+//	ft_putstr("c ");
+//	ft_putnbr(*reverse);
+	ft_putstr("commands next ");
+	ft_putnbr(cur_max->val);
+	ft_putstr("commands reverse ");
+	ft_putnbr(elem->val);
+//	destroy_iter(&iter);
+//	ft_putstr("commands next ");
+//	ft_putnbr(*next);
+//	ft_putstr("commands reverse ");
+//	ft_putnbr(*reverse);
 	destroy_iter(&iter);
 }
+
+void			count_commands_b(unsigned int *rota, unsigned int *rrota, t_stack *b, t_node *elem)
+{
+	t_iter				*iter;
+	unsigned int		i;
+	unsigned int		min;
+
+	i = 0;
+	iter = new_iter(b);
+	while (i < b->size && iter->cur != elem)
+	{
+		if (i >= b->size / 2)
+			*rota = *rota + 1;
+		else
+			*rrota = *rrota + 1;
+		iter->next_iter(iter);
+		i++;
+	}
+	destroy_iter(&iter);
+	ft_putstr("count_commands: ");
+	ft_putendl("");
+	if (*rota < *rrota)
+		*rrota = 0;
+	else
+		*rota = 0;
+}
+
 
 unsigned int			count_element(t_stack *a, t_stack *b, t_node *node)
 {
@@ -84,7 +129,7 @@ unsigned int			count_element(t_stack *a, t_stack *b, t_node *node)
 	rb_count = 0;
 	rrb_count = 0;
 	count_commands(&ra_count, &rra_count, a, node);
-	count_commands(&rb_count, &rrb_count, b, node);
+//	count_commands_b(&rb_count, &rrb_count, b, node);
 	return (ra_count + rra_count + rb_count + rrb_count);
 }
 
@@ -98,16 +143,22 @@ t_node		*find_list(t_stack *a, t_stack *b)
 	unsigned int		i;
 	unsigned int		min;
 	unsigned int		cur;
+	unsigned int		rot;
 	t_node				*res;
 
 	i = 0;
+	rot = 0;
 	iter = new_iter(b);
 	cur = MAX_INT;
 	min = MAX_INT;
 	res = b->begin;
 	while (i < b->size)
 	{
-		cur = count_element(a, b, iter->cur);
+		cur = count_element(a, b, iter->cur) + rot;
+		if (i >= b->size / 2)
+			rot--;
+		else
+			rot++;
 		if (cur < min)
 		{
 			min = cur;
@@ -137,8 +188,8 @@ void		choose_element(t_stack *a, t_stack *b)
 	rrb_count = 0;
 	lst = find_list(a, b);
 	count_commands(&ra_count, &rra_count, a, lst);
-	count_commands(&rb_count, &rrb_count, b, lst);
-
+//	count_commands(&rb_count, &rrb_count, b, lst);
+	count_commands_b(&rb_count, &rrb_count, b, lst);
 //	perform_both_commands(both_commands(&ra_count, &rb_count), a, b, &rr);
 //	perform_both_commands(both_commands(&rra_count, &rrb_count), a, b, &rrr);
 	perform_single_command(ra_count, a, &ra);
