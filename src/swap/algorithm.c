45fcccc8e4fbe73543e546d5a7d8371c4a66f410
Hugor Chau <hchau@su-a4.msk.21-school.ru>
2021-04-25 18:32:49 +0300

    algorithm is not so good, but it works

--- src/swap/algorithm.c
+++ src/swap/algorithm.c
@@ -1,5 +1,17 @@
 #include "header.h"
 
+void	get_a_sorted(t_stack *a)
+{
+	void		(*rotate_a)(t_stack *);
+
+	if (a->begin->index >= a->size)
+		rotate_a = &rra;
+	else
+		rotate_a = &ra;
+	while (a->begin->index != 0)
+		rotate_a(a);
+}
+
 void	main_cycle(t_stack *a, t_stack *b)
 {
 	unsigned int			main_size;
@@ -8,4 +20,5 @@ void	main_cycle(t_stack *a, t_stack *b)
 	indexing(a);
 	prepare_a(a, b);
 	get_from_b(a, b);
+	get_a_sorted(a);
 }
